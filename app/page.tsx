69c3cf5f097932217823fc4c1a00a3d8222d5f98
Debarpan Chakraborty <madhumitachakraborty.mid@gmail.com>
2026-07-23 03:50:54 +0530

    Change Supabase client import to server version

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { createClientBrowser } from '@/utils/supabase';
+import { createClient } from '@/utils/supabase/server';
 import CheckoutButton from '@/components/CheckoutButton';
 
 type StudyMaterial = {
